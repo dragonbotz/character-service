01ccf6ec94ca36903bb035e98a6309a804fb05b9
Lahcène Belhadi <lahcene.belhadi@gmail.com>
2023-05-20 12:04:48 +0200

    Removes lib

--- lib/dbzlib-rs/src/model/mod.rs
+++
@@ -1,6 +0,0 @@
-//! Model module
-//!
-//! This module contains the implementation of all Dragon Bot Z models.
-//!
-//! Authors: Lahcène Belhadi <lahcene.belhadi@gmail.com>
-pub mod character;
--- lib/dbzlib-rs/src/util/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Utility module
-//!
-//! This module contains all Dragon Bot Z's utilities
-//!
-//! Authors: Lahcène Belhadi <lahcene.belhadi@gmail.com>
-
-pub mod error;
